fbafef65201d6c56a6c535c296a03e288ed5df27
yumosx <zhengel2022@163.com>
2025-10-07 09:28:43 +0800

    add the pydantic

--- pydantic/user.py
+++ pydantic/user.py
@@ -1,7 +1,27 @@
-from pydantic import BaseModel, Field
+from typing import Any, Self
+
+from pydantic import BaseModel, Field, field_validator, model_validator
 from pydantic.v1 import EmailStr
 
+VALID_NAME_REGEX = ""
 
 class User(BaseModel):
     name: str = Field(examples=["Arjain"])
     email: EmailStr = Field(examples=["@example@arjancodes.com"])
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value: str) -> str:
+        if not VALID_NAME_REGEX.match(value):
+            raise ValueError("")
+        return value
+
+    @field_validator("name", mode="before")
+    @classmethod
+    def validate_name(cls, value:str) -> str:
+        # 表示在数据验证之前
+        if isinstance(value, str):
+            return value.replace(',', '')
+        return value
+
+    @model_validator(mode="before")
